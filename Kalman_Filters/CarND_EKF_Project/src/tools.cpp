c994f61d684cf82f5ec21f5b8f6e1d17d9681b05
Baris Kara <baris.can.kara@nxp.com>
2018-02-01 10:54:09 +0100

    m

--- Kalman_Filters/CarND_EKF_Project/src/tools.cpp
+++ Kalman_Filters/CarND_EKF_Project/src/tools.cpp
@@ -53,6 +53,7 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
     * Calculate a Jacobian here.
   */
 	MatrixXd Hj(3, 4);
+	Hj << 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
 
 	if (x_state.size() != 4) {
 		cout << "ERROR - CalculateJacobian () - The state vector must have size 4." << endl;
