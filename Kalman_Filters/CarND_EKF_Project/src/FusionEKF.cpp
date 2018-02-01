f428365352cd0a7abccbd144f1a615d2e4c6f10b
Baris Kara <baris.can.kara@nxp.com>
2018-02-01 11:24:57 +0100

    m

--- Kalman_Filters/CarND_EKF_Project/src/FusionEKF.cpp
+++ Kalman_Filters/CarND_EKF_Project/src/FusionEKF.cpp
@@ -65,10 +65,16 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       * Remember: you'll need to convert radar from polar to cartesian coordinates.
     */
     // first measurement
-	previous_timestamp_ = 0;
+	
     cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
 	ekf_.x_ <<	1,1,1,1 ;
+
+	ekf_.F_ = MatrixXd(4, 4);
+	ekf_.F_ << 1, 0, 1, 0,
+		0, 1, 0, 1,
+		0, 0, 1, 0,
+		0, 0, 0, 1;
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       /**
@@ -92,7 +98,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 		ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1],
 			0, 0;
     }
-
+	previous_timestamp_ = measurement_pack.timestamp_;
     // done initializing, no need to predict or update
     is_initialized_ = true;
     return;
@@ -111,13 +117,13 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    */
   double noise_ax = 9.0;
   double noise_ay = 9.0;
-
+  
   float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
   previous_timestamp_ = measurement_pack.timestamp_;
 
   //Modify the F matrix so that the time is integrated
   // State transition matrix update
-  ekf_.F_ = MatrixXd(4, 4);
+  
   ekf_.F_(0, 2) = dt;
   ekf_.F_(1, 3) = dt;
   float dt_2 = dt * dt;
