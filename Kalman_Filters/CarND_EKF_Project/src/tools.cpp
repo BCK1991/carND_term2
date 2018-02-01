3c4c1391051dd8b6fcb2420f6e3666338efdb55c
Baris Kara <baris.can.kara@nxp.com>
2018-02-01 14:43:42 +0100

    updating RMSE calculation

--- Kalman_Filters/CarND_EKF_Project/src/tools.cpp
+++ Kalman_Filters/CarND_EKF_Project/src/tools.cpp
@@ -18,32 +18,30 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 	VectorXd rmse(4);
 	rmse << 0, 0, 0, 0;
 
-	// check the validity of the following inputs:
-	//  * the estimation vector size should not be zero
-	//  * the estimation vector size should equal ground truth vector size
-	if (estimations.size() != ground_truth.size()
-		|| estimations.size() == 0){
-		cout << "Invalid estimation or ground_truth data" << endl;
+	if (estimations.size() == 0){
+		cout << "ERROR - CalculateRMSE () - The estimations vector is empty" << endl;
 		return rmse;
 	}
 
-	//accumulate squared residuals
-	for (unsigned int i = 0; i < estimations.size(); ++i){
-
-		VectorXd residual = estimations[i] - ground_truth[i];
-
-		//coefficient-wise multiplication
-		residual = residual.array()*residual.array();
-		rmse += residual;
+	if (ground_truth.size() == 0){
+		cout << "ERROR - CalculateRMSE () - The ground-truth vector is empty" << endl;
+		return rmse;
 	}
 
-	//calculate the mean
-	rmse = rmse / estimations.size();
+	unsigned int n = estimations.size();
+	if (n != ground_truth.size()){
+		cout << "ERROR - CalculateRMSE () - The ground-truth and estimations vectors must have the same size." << endl;
+		return rmse;
+	}
 
-	//calculate the squared root
+	for (unsigned int i = 0; i < estimations.size(); ++i){
+		VectorXd diff = estimations[i] - ground_truth[i];
+		diff = diff.array()*diff.array();
+		rmse += diff;
+	}
+
+	rmse = rmse / n;
 	rmse = rmse.array().sqrt();
-
-	//return the result
 	return rmse;
 }
 
