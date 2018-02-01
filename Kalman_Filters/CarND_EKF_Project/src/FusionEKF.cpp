cb9a26936e9c3bdef65a64c93a0a3d86df1b8b4b
Baris Kara <baris.can.kara@nxp.com>
2018-02-01 09:11:58 +0100

    f

--- Kalman_Filters/CarND_EKF_Project/src/FusionEKF.cpp
+++ Kalman_Filters/CarND_EKF_Project/src/FusionEKF.cpp
@@ -118,7 +118,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   // State transition matrix update
   ekf_.F_ = MatrixXd(4, 4)
 	  kf_.F_(0, 2) = dt;
-  kf_.F_(1, 3) = dt;
+  ekf_.F_(1, 3) = dt;
   float dt_2 = dt * dt;
   float dt_3 = dt_2 * dt;
   float dt_4 = dt_3 * dt;
