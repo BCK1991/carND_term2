a9e5860fcf62fcef0af947f9f3ffbdd6895bc94a
Baris Kara <baris.can.kara@nxp.com>
2018-02-01 09:14:47 +0100

    m

--- Kalman_Filters/CarND_EKF_Project/src/FusionEKF.cpp
+++ Kalman_Filters/CarND_EKF_Project/src/FusionEKF.cpp
@@ -116,7 +116,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   //Modify the F matrix so that the time is integrated
   // State transition matrix update
-  ekf_.F_ = MatrixXd(4, 4)
+  ekf_.F_ = MatrixXd(4, 4);
   ekf_.F_(0, 2) = dt;
   ekf_.F_(1, 3) = dt;
   float dt_2 = dt * dt;
