d4c431e90a69ba1021c0fe0adb573b4fcb3a80e9
Baris Kara <baris.can.kara@nxp.com>
2018-02-16 11:58:50 +0100

    debugging

--- Kalman_Filters/CarND_UKF_Project/src/ukf.cpp
+++ Kalman_Filters/CarND_UKF_Project/src/ukf.cpp
@@ -403,16 +403,19 @@ void UKF::UpdateCommon(MeasurementPackage meas_package, MatrixXd Zsig, int n_z_)
 
 		//residual
 		VectorXd z_diff = Zsig.col(i) - z_pred_;
-		//angle normalization
-		while (z_diff(1)> M_PI) z_diff(1) -= 2.*M_PI;
-		while (z_diff(1)<-M_PI) z_diff(1) += 2.*M_PI;
-
 		// state difference
 		VectorXd x_diff = Xsig_pred_.col(i) - x_;
-		//angle normalization
-		while (x_diff(3)> M_PI) x_diff(3) -= 2.*M_PI;
-		while (x_diff(3)<-M_PI) x_diff(3) += 2.*M_PI;
 
+		//angle normalization
+		if (sensor_type == MeasurementPackage::RADAR){
+			while (z_diff(i) > M_PI) z_diff(1) -= 2.*M_PI;
+			while (z_diff(1) < -M_PI) z_diff(1) += 2.*M_PI;
+
+
+			//angle normalization
+			while (x_diff(3) > M_PI) x_diff(3) -= 2.*M_PI;
+			while (x_diff(3) < -M_PI) x_diff(3) += 2.*M_PI;
+		}
 		Tc = Tc + weights_(i) * x_diff * z_diff.transpose();
 	}
 	std::cout << "Tc updated :" << Tc << std::endl;
