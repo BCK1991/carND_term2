5c209ea37d2023d10d4ac57f5aad7a138103541c
Baris Kara <baris.can.kara@nxp.com>
2018-05-31 20:07:21 +0200

    bug fixes

--- PID_Control/src/main.cpp
+++ PID_Control/src/main.cpp
@@ -37,8 +37,8 @@ int main()
   //Based on the discussions in the Udacity Forum, the (rough) initial estimates are done.
   //pid.Init(0.25, 0.005, 2.0);  -> overshoot observed, counterbalancing with increasing D
   //pid.Init(0.25, 0.005, 3.0); -> overshoot observed, counterbalancing with decreasing P
-  pid.Init(0.15, 0.005, 3.0);
-
+  //pid.Init(0.15, 0.005, 3.0); ->overshoot observed, counterbalancing with decreasing P and decreasing I
+  pid.Init(0.12, 0.001, 3.0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
