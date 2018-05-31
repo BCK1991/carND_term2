3cb583f06ba4e243090efa990c8c0a03d0ec63eb
Baris Kara <baris.can.kara@nxp.com>
2018-05-31 20:01:14 +0200

    bug fixes

--- PID_Control/src/main.cpp
+++ PID_Control/src/main.cpp
@@ -35,8 +35,8 @@ int main()
   PID pid;
   // TODO: Initialize the pid variable.
   //Based on the discussions in the Udacity Forum, the initial estimates are done.
-  pid.Init(0.25, 0.005, 2.0);
-
+  //pid.Init(0.25, 0.005, 2.0);  -> overshoot observed, counterbalancing with increasing D
+  pid.Init(0.25, 0.005, 3.0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
