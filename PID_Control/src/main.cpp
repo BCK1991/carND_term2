8a5b9b90a6d3c21d0249ea38c6e166adad474adc
Baris Kara <baris.can.kara@nxp.com>
2018-06-01 09:46:13 +0200

    bug fixes

--- PID_Control/src/main.cpp
+++ PID_Control/src/main.cpp
@@ -41,7 +41,8 @@ int main()
   //pid.Init(0.12, 0.001, 3.0); -> see report for twiddle values 
   //pid.Init(0.132, 0.001, 3.3); ->see report for twiddle values 
   //pid.Init(0.158, 0.001, 3.63); -> see report for twiddle values
-  pid.Init(0.14, 0.001, 3.5);
+  //pid.Init(0.14, 0.001, 3.5);
+  pid.Init(0.145, 0.001, 3.53);
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -68,7 +69,7 @@ int main()
           // DEBUG
           //std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 		  pid.UpdateError(cte);
-		  pid.TotalError(cte, true,true);
+		  pid.TotalError(cte, true, false);
 		  steer_value = -pid.p_error * pid.Kp - pid.i_error * pid.Ki - pid.d_error * pid.Kd;
 		  if (steer_value > 1) {
 			  steer_value = 1;
